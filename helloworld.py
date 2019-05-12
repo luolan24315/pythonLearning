266b05e28ba662e311a7d3be61d7ee9886c881e8
rongjinjin <rong_jinjin@163.com>
2019-05-12 13:55:09 +0800

    练习1

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+# _*_coding: utf-8_*_
+# Author:rongjinjin
+# Time:20190226
+
+print("hello world!")
---
+++ weatherExercise.py
@@ -0,0 +1,5 @@
+# _*_coding: utf-8_*_
+# Author:rongjinjin
+# Time:20190226
+print("*杭州天气预报*")
+print("2019年2月26日")
